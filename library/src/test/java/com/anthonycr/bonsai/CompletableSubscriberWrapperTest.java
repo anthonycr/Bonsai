c8725a7799368799558d26c3b63002d7fb84cfbf
anthony restaino <anthonyrestaino11@gmail.com>
2017-03-30 22:24:53 -0400

    Adding test case for calling onComplete after onError

--- library/src/test/java/com/anthonycr/bonsai/CompletableSubscriberWrapperTest.java
+++ library/src/test/java/com/anthonycr/bonsai/CompletableSubscriberWrapperTest.java
@@ -51,6 +51,20 @@ public class CompletableSubscriberWrapperTest extends BaseUnitTest {
         Mockito.verifyNoMoreInteractions(completableOnSubscribe);
     }
 
+    @Test
+    public void onErrorTest_onCompleteNotCalled() throws Exception {
+        CompletableSubscriberWrapper<CompletableOnSubscribe> wrapper = new CompletableSubscriberWrapper<>(completableOnSubscribe, null, Schedulers.current());
+
+        Exception exception = new Exception("Test exception");
+
+        wrapper.onError(exception);
+        wrapper.onComplete();
+
+        Mockito.verify(completableOnSubscribe).onError(exception);
+
+        Mockito.verifyNoMoreInteractions(completableOnSubscribe);
+    }
+
     @Test(expected = RuntimeException.class)
     public void onErrorTest_throwsException_notOverridden() throws Exception {
         CompletableOnSubscribe onSubscribe = new CompletableOnSubscribe() {
